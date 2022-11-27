09d7f388020bf9ec2566cd855935c011a81396a9
Binay9 <soundofpeace.bd@gmail.com>
2022-11-27 15:45:04 +0000

    initial commit

---
+++ config/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    'site_url' => 'http://localhost/mvc/',
+    'db_host' => 'localhost',
+    'db_user' => 'admin',
+    'db_pass' => 'admin9090',
+    'db_name' => 'mvc',
+    'timezone' => 'Europe/Lisbon'
+];
