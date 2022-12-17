9414d07b331d6dadf2be64fdb77416b2b972c1e8
Binay9 <soundofpeace.bd@gmail.com>
2022-12-17 03:46:20 +0000

    front page template

--- views/front/pages/index.php
+++ views/front/pages/index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="<?php echo url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo url('assets/css/all.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo url('assets/css/front.css'); ?>" rel="stylesheet">
 </head>
 
@@ -15,19 +16,234 @@
 
         <div class="row">
             <div class="col-12">
-                <div class="row">
-                    <div class="col-auto me-auto my-3">
-                        <span class="site-name">NewsBlog</span>
+
+                <div class="row my-3">
+                    <div class="col me-auto">
+                        <a class="site-name" href="<?php echo url(); ?>">NewsBlog</a>
                     </div>
-                    <div class="col-4 my-3">
-                        <div class="input-group">
-                            <input type="text" name="search" class="form-control" placeholder="Search" required>
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="submit">Search</button>
+                    <div class="col-4 pt-2">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search">
+                            <button class="btn btn-outline-secondary" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary col-12">
+                        <div class="container-fluid">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-12 my-3">
+
+                        <div class="row">
+                            <div class="col-12 mb-3 top-article-title">
+                                This is a demo title.
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                            </div>
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-12 pb-2">
+                                        <span class="top-article-info"> Author: Person A</span>
+                                        <span class="top-article-info"> Published At: 15 May 2019 12:00 AM </span>
+                                    </div>
+                                    <div class="col-12">
+                                        fjd ds sdf sdfsd sdfsdfsd sdfsd df dd ds a sfd asf.
+                                        sdfd aa sd safjn jfsd this isi a demo text. fjd ds sdf sdfsd sdfsdfsd sdfsd df dd ds a sfd asf. sdfd aa sd safjn jfsd this isi a demo text.
+                                        fjd ds sdf sdfsd sdfsdfsd sdfsd df dd ds a sfd asf.
+                                        sdfd aa sd safjn jfsd this isi a demo text.
+                                        fjd ds sdf sdfsd sdfsdfsd sdfsd df dd ds a sfd asf.
+                                        sdfd aa sd safjn jfsd this isi a demo text. fjd ds sdf sdfsd sdfsdfsd sdfsd df dd ds a sfd asf. sdfd aa sd safjn jfsd this isi a demo text.
+                                        fjd ds sdf sdfsd sdfsdfsd sdfsd df dd ds a sfd asf.
+                                        sdfd aa sd safjn jfsd this isi a demo text.
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#" class="btn btn-secondary btn-sm">Read More</a>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <!-- Hr Line -->
+                <div class="row">
+                    <div class="col-12">
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 my-3">
+
+                        <div class="row">
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3 mb-3">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <img src="<?php echo url('assets/images/Nature-Picture.jpg') ?>" class="mx-auto d-block img-fluid border">
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#">This is a demo title.</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 bg-secondary text-white py-3 footer">
+                        <div class="row">
+
+                            <div class="col-4">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a class="site-name" href="<?php echo url(); ?>">NewsBlog</a>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    Lisbon, Portugal<br>
+                                    Email: <a href="mailto:info@newsblog.com">info@newsblog.com</a><br>
+                                    Phone: 987654321
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h4>Find Us On</h4>
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="http://facebook.com" class="social-icon" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                                        <a href="http://instagram.com" class="social-icon" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                                        <a href="http://linkedin.com" class="social-icon" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                                        <a href="http://whatsapp.com" class="social-icon" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h4>Contact Us</h4>
+                                    </div>
+                                    <div class="col-12">
+                                        <form action="">
+                                            <div class="mb-2">
+                                                <input class="form-control" type="text" name="name" placeholder="Enter your name" required>
+                                            </div>
+                                            <div class="mb-2">
+                                                <input class="form-control" type="email" name="email" placeholder="Enter your email" required>
+                                            </div>
+                                            <div class="mb-2">
+                                                <textarea name="message" class="form-control" placeholder="Enter your message" required></textarea>
+                                            </div>
+
+                                            <div class="mb-2">
+                                                <button class="btn btn-light btn-sm">Send</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
             </div>
         </div>
 
